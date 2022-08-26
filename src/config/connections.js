db31396e8574fff190b734272c14840b4656fe8a
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-08-26 19:38:09 +0100

    fixed env variables

--- src/config/connections.js
+++ src/config/connections.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const options = {
-  host: DB_HOST,
+  host: process.env.DB_HOST,
   dialect: 'mysql',
-  port: 3306,
+  port: process.env.PORT || 3306,
   logging: false,
 };
 
